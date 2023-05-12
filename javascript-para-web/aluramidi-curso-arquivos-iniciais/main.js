04cb44127865023b14255d73a7f4e6bbc6c2477a
murilloressineti <murillobr22@gmail.com>
2023-05-12 16:03:12 -0300

    Referências

--- javascript-para-web/aluramidi-curso-arquivos-iniciais/main.js
+++ javascript-para-web/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,5 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
